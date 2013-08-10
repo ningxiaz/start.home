1346dbc928bd38ab56bd1f020e789cd450573da5
Ningxia Zhang <thmmrs2298@gmail.com>
2013-08-09 19:45:39 -0700

    minor changes on floorplan

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -99,7 +99,7 @@ $(document).ready(function(){
 
 function show_now(){
 	$('#timeline').fadeIn();
+	$('#floorplan').append("<svg id=\"floorplan_svg\" height=\"400\" width=\"400\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" ><image x=\"0\" y=\"0\" height=\"400\" width=\"400\"  xlink:href=\"images/floorplan.svg\" /></svg>");
 	FloorPlan.init();
 	FloorPlan.draw_rects();
-	$('#floorplan').append("<svg id=\"floorplan_svg\" height=\"400\" width=\"400\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" ><image x=\"0\" y=\"0\" height=\"400\" width=\"400\"  xlink:href=\"images/floorplan.svg\" /></svg>");
 }
